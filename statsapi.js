e39f9b32d09250f8467d9b2af2bead0d4e935738
Rv-oo <muhammadrivoilva@gmail.com>
2025-08-19 08:42:43 +0700

    Update statsapi.js

--- statsapi.js
+++ statsapi.js
@@ -1,13 +1,1 @@
-export default async function handler(req, res) {
-  try {
-    // Ambil dari API asli (HTTP)
-    const response = await fetch("http://uk-01.rrhosting.eu:1299/api/server-stats");
-    const data = await response.json();
 
-    // Kirim balik ke browser lewat HTTPS
-    res.status(200).json(data);
-  } catch (error) {
-    console.error("Proxy error:", error);
-    res.status(500).json({ error: "Failed to fetch server stats" });
-  }
-}
