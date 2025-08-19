aaaee034a1987f140874f0d1b3133624cf8f402d
Rv-oo <muhammadrivoilva@gmail.com>
2025-08-19 09:25:46 +0700

    Update server-stats.js

--- api/server-stats.js
+++ api/server-stats.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch(`${process.env.BOT_API_URL}/api/server-stats`);
+    const response = await fetch(`http://uk-01.rrhosting.eu:1299/api/server-stats`);
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
@@ -8,4 +8,5 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Gagal ambil data server" });
   }
 }
+
 
